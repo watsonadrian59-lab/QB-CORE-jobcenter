b63f53e683ef4a8a2139bca5b180b303cf51cd23
Hayabusa <watsonadrian59@gmail.com>
2026-02-28 05:38:40 -0500

    Add files via upload

---
+++ Hayabusa_jobs/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'qb-jobcenter'
+description 'Modern City Job Application UI'
+author 'YourServer'
+version '2.0.0'
+
+ui_page 'html/index.html'
+
+shared_script 'config.lua'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+dependencies {
+    'qb-core',
+    'qb-target'
+}
